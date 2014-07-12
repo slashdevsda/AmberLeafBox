d14146e510f34b439a9e4a9cb6773bdee17eb72c
ecuer-thomas <thomas.ecuer@epitech.eu>
2014-07-12 17:12:52 +0200

    main file

---
+++ alb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-#
+#
+# AmberLeafBox
+# Soup - 2014
+#
+
+from sys import path
+path.append("./src/")
+from main import main
+
+if __name__ == '__main__':
+    main()
