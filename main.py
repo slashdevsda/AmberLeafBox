9abc30a946423210680f07135d05edcc2cb3db8c
ecuer-thomas <thomas.ecuer@epitech.eu>
2014-07-12 02:56:56 +0200

    topbar, save & quit menu options

--- main.py
+++ main.py
@@ -23,11 +23,7 @@ class App(object):
         self.sw.add(self.view)
         
         self.win = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.win.add(self.sw)
         self.win.resize(800, 600)
-
-
-        self.win.show_all()
 
         self.em = EventManager()
         if self._options.replay:
@@ -43,12 +39,22 @@ class App(object):
             )
 
 
-        self.view.open("http://127.0.0.1:3000/")
+        #self.view.open("http://127.0.0.1:3000/")
+        self.view.open("http://leofaber.tumblr.com/")
+        vbox = gtk.VBox(False)
+        menu = self.create_menu()
+        vbox.pack_start(menu, False, False, 0)
+        vbox.pack_start(self.sw, True, True, 0)
 
+
+        self.win.add(vbox)
+        vbox.show()
+        self.win.show_all()
         try:
             gtk.main()
         except KeyboardInterrupt:
-            self.em.dump(self._options.filename)
+            if self._options.record:
+                self.em.dump(self._options.filename)
 
     def parse_args(self):
         parser = OptionParser()
@@ -71,11 +77,71 @@ class App(object):
             default="events.pk",
             help="write/read events from [filename]."
         )
-        
+
         self._options, args = parser.parse_args()
 
         return  (self._options, args)
-        
+
+    def save_session(self, *args):
+        if self._options.record:
+            self.em.dump(self._options.filename)
+        print("session saved!")
+        return 0
+
+    def quit(self, *args):
+        gtk.main_quit()
+        return 0
+
+    def create_menu(self):
+        """
+        UI Stuff, again
+        from http://www.pygtk.org/pygtk2tutorial/ch-MenuWidget.html
+        """
+        file_item = gtk.MenuItem("File")
+        menu_bar = gtk.MenuBar()
+        file_menu = gtk.Menu()
+        quit_item = gtk.MenuItem("quit without saving")
+        save_item = gtk.MenuItem("Save session")
+        save_quit_item = gtk.MenuItem("Save session and quit...")
+
+        # Add them to the menu
+        file_menu.append(save_quit_item)
+        file_menu.append(save_item)
+        file_menu.append(quit_item)
+                
+        save_item.connect_object(
+            "activate",
+            self.save_session,
+            "save_session"
+        )
+
+        save_item.connect_object(
+            "activate",
+            self.save_session,
+            "save"
+        )
+
+        save_quit_item.connect_object(
+            "activate",
+            lambda x : self.save_session() + self.quit(),
+            "quit"
+        )
+
+        # We can attach the Quit menu item to our exit function
+        #quit_item.connect_object ("activate", destroy, "file.quit")
+        save_quit_item.show()
+        save_item.show()
+        quit_item.show()
+
+        menu_bar.append(file_item)
+        file_item.set_submenu(file_menu)
+        file_item.show()
+
+        menu_bar.show()
+
+        print("=" * 60)
+        return menu_bar
+
 if __name__ == '__main__':
     app = App()
     app.parse_args()
